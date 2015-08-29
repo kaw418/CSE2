0116cd10b2cae81a8589e77210f720bdd3005dfd
kaw418 <kaw418@lehigh.edu>
2015-08-29 21:35:41 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//////////////////////////////////////////////////////
+//Katherine Wu
+//Demo 0
+//Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the statement with box around it
+    System.out.println("  -------------");
+    System.out.println("  |  WELCOME  |");
+    System.out.println("  -------------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-K--A--W--4--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I love baking chocolate chip cookies and oatmeal raisin cookies in the winter.");
+    System.out.println("They're perfect with a mug of hot cocoa by the fireplace.");
+    
+    //test/testtest
+    }
+}
